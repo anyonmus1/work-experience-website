502bf071be45cd6743665cabbba34db266976172
Your Name <you@example.com>
2024-12-15 18:47:14 -0700

    typescrpit conflict resolved

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'stream-browserify';
-declare module 'assert';
-declare module 'stream-http';
-declare module 'https-browserify';
-declare module 'os-browserify';
-declare module 'url';
-declare module 'crypto-browserify';
-declare module 'path-browserify';
-declare module 'browserify-zlib';
---
+++ src/global.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'stream-browserify': {},
+    'assert': {},
+    'stream-http': {},
+    'https-browserify': {},
+    'os-browserify': {},
+    'url': {},
+    'crypto-browserify': {},
+    'path-browserify': {},
+    'browserify-zlib': {}
+  };
